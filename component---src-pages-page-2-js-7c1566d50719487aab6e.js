7a8d95fcf3c88d5d53b701e13f3ccf2cb6b0b8a1
DrewTJohnson <dtjohnson1995@gmail.com>
2020-09-26 13:02:37 -0400

    Updates

---
+++ component---src-pages-page-2-js-7c1566d50719487aab6e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{p5nM:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),c=t.n(a);t("Wbzz");e.default=function(){return c.a.createElement("p",null,"Second Page")}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-7c1566d50719487aab6e.js.map
