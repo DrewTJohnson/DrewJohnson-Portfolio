54b673a2ba2609d2536b08c7aac531090a5823fc
DrewTJohnson <dtjohnson1995@gmail.com>
2020-09-26 13:42:25 -0400

    Added open links to new tab

--- src/components/homeSkills.js
+++ src/components/homeSkills.js
@@ -22,7 +22,7 @@ const homeSkills = () => {
                 <div className="flex-grid">
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
+                            <Link to="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank">
                                 <img src={HTML} title="HTML 5" alt="HTML 5 Logo, links to MDN docs." />
                                 <div className="popup" id="popup1">HTML5</div>
                             </Link>
@@ -30,7 +30,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://developer.mozilla.org/en-US/docs/Web/CSS">
+                            <Link to="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
                                 <img src={CSS} title="CSS 3" alt="CSS3 Logo, links to MDN docs." />
                                 <div className="popup" id="popup2">CSS3</div>
                             </Link>
@@ -38,7 +38,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://www.javascript.com/">
+                            <Link to="https://www.javascript.com/" target="_blank">
                                 <img src={JS} title="JavaScript" alt="JavaScript Logo, links to JavaScript website." />
                                 <div className="popup" id="popup3">JavaScript</div>
                             </Link>
@@ -46,7 +46,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://getbootstrap.com/">
+                            <Link to="https://getbootstrap.com/" target="_blank">
                                 <img src={BOOTSTRAP} title="Bootstrap" alt="Bootstrap Logo, links to Bootstrap website." />
                                 <div className="popup" id="popup4">Bootstrap</div>
                             </Link>
@@ -54,7 +54,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://www.gatsbyjs.com/">
+                            <Link to="https://www.gatsbyjs.com/" target="_blank">
                                 <img src={GATSBY} title="GatsbyJS" alt="GatsbyJS Logo, links to GatsbyJS webiste." />
                                 <div className="popup" id="popup5">GatsbyJS</div>
                             </Link>
@@ -66,7 +66,7 @@ const homeSkills = () => {
                         <div className="flex-grid">
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://www.ruby-lang.org/en/">
+                            <Link to="https://www.ruby-lang.org/en/" target="_blank">
                                 <img src={RUBY} title="Ruby" alt="Ruby Logo, links to Ruby website." />
                                 <div className="popup" id="popup6">Ruby</div>
                             </Link>
@@ -74,7 +74,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://rubyonrails.org/">
+                            <Link to="https://rubyonrails.org/" target="_blank">
                                 <img src={RAILS} title="Rails" alt="Rails Logo, links to Ruby on Rails website." />
                                 <div className="popup" id="popup7">Rails</div>
                             </Link>
@@ -82,7 +82,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://reactjs.org/">
+                            <Link to="https://reactjs.org/" target="_blank">
                                 <img src={REACT} title="ReactJS" alt="ReactJS Logo, links to ReactJS website." />
                                 <div className="popup" id="popup8">ReactJS</div>
                             </Link>
@@ -90,7 +90,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://kotlinlang.org/">
+                            <Link to="https://kotlinlang.org/" target="_blank">
                                 <img src={KOTLIN} title="Kotlin" alt="Kotlin Logo, links to Kotlin webiste." />
                                 <div className="popup" id="popup9">Kotlin</div>
                             </Link>
@@ -98,7 +98,7 @@ const homeSkills = () => {
                     </div>
                     <div className="col">
                         <div className="image-container">
-                            <Link to="https://git-scm.com/">
+                            <Link to="https://git-scm.com/" target="_blank">
                                 <img src={GIT} tite="Git" alt="Git Logo, links to Git website." />
                                 <div className="popup" id="popup10">GIT</div>
                             </Link>
